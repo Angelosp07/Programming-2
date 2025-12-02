eb15e5cd8deab54619053d67020b311ce635553f
Angelosp07 <ap1315@exeter.ac.uk>
2025-12-02 01:26:20 +0000

    print_board done

--- components.py
+++ components.py
@@ -1,24 +1,40 @@
 def initialise_board(size=8):
-    L = "Light"
-    D = "Dark "
-    N = "None "
-
     board = []
 
     for i in range(size):
         row = []
 
         for j in range(size):
-
-            row.append(N)
+            row.append("None ")
         board.append(row)
 
 
-    board[3][3] = L
-    board[3][4] = D
-    board[4][3] = D
-    board[4][4] = L
+    board[3][3] = "Light"
+    board[3][4] = "Dark "
+    board[4][3] = "Dark "
+    board[4][4] = "Light"
     return board
 
-b = initialise_board()
-print(b)
+
+#Create a function called print_board that accepts a board as an argument and prints an ascii representation of the board state to the command line.
+
+def print_board(board):
+
+    for row in board:
+        R = []
+
+        for square in row:
+            if square == "None ":
+                R.append("[ ]")
+            elif square == "Light":
+                R.append("[O]")
+            elif square == "Dark ":
+                R.append("[*]")
+        
+        print("".join(R))
+
+
+
+
+
+print_board(initialise_board())
