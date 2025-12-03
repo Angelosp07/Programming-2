62557ef030e59ee7e7f8ba800d591224777f1eba
Angelosp07 <ap1315@exeter.ac.uk>
2025-12-03 18:53:24 +0000

    try except added to cli_coords_input()

--- game_engine.py
+++ game_engine.py
@@ -8,12 +8,21 @@ def cli_coords_input():
     while x < 1 or x > 8 :
         print("Input the x coordinate")
 
-        x = int(input())
+        try:
+            x = int(input())
+        except ValueError:
+            print("Not an integer between 1-8 inclusive")
+            x = 0
 
     while y < 1 or y > 8:
         print("Input the y coordinate")
+
+        try:
+            y = int(input())
+        except ValueError:
+            print("Not an integer between 1-8 inclusive")
+            y = 0
         
-        y = int(input())
 
     return (x,y)
 
